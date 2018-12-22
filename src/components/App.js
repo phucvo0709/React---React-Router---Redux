8760adf0d0aba27c87a7372e5d9a2c6723941cc4
Phúc Võ <phuc@MacBook-Air-cua-Phuc.local>
2018-12-22 08:28:09 +0700

    add default component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import PublicLayout from './Layouts/PublicLayout';
+
+import Home from './Home';
+import NotFound from './NotFound';
 
 class App extends Component {
   render() {
+    const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
+      <Route {...rest} render={props => (
+        <Layout>
+          <Component {...props}/>
+        </Layout>
+      )} />
+    )
+
     return (
       <div>
-        <h1>Hello new project</h1>
+        <Switch>
+          <AppRoute exact path="/" layout={PublicLayout} component={Home}/>
+          <AppRoute layout={PublicLayout} component={NotFound} />
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-center m-4">Trang chủ</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
