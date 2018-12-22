ae587ef1fe0391d49f6aea273b7f348d2a29b7b0
Phúc Võ <phuc@MacBook-Air-cua-Phuc.local>
2018-12-22 08:15:36 +0700

    connect all to source

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// auth
+    // import setAuthToken from './utils/setAuthToken';
+// router
+import { Router } from 'react-router-dom';
+// redux
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+//reducer
+import errorReducer from './store/reducers/errorReducer';
+import successReducer from './store/reducers/successReducer';
+//plugins
+import moment from 'moment';
+import Moment from 'react-moment';
+import createHistory from 'history/createBrowserHistory'
+// 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+const history = createHistory()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Sets the moment instance to use.
+Moment.globalMoment = moment;
+Moment.globalLocale = 'vi';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// redux tools extension google
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// root reducer
+const rootReducer = combineReducers({
+    errors: errorReducer,
+    success: successReducer
+})
+// create store redux 
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+//app
+const app = (
+    <Provider store={store}>
+        <Router history={history}>
+            <App />
+        </Router>
+    </Provider>
+);
+ReactDOM.render(app, document.getElementById('root'));
 serviceWorker.unregister();
